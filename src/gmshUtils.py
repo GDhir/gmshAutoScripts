e27dbbb78e706e43dadae8daab90c77692938bb0
Gaurav <mail2gauravdhir@gmail.com>
2023-11-20 12:15:13 -0700

    3D Layers and Zones for making tet/pyramid connections

--- src/gmshUtils.py
+++ src/gmshUtils.py
@@ -75,26 +75,165 @@ def recombineSurfaces( planeIds ):
 
     [ gmsh.model.mesh.setRecombine( 2, id ) for id in planeIds ]
 
-class Zone:
+def recombineZones( zones ):
 
-    def __init__( self, pointSet, xoffset, yoffset ):
+    for zone in zones:
+
+        surfaceVals = zone.surfaces
+        recombineSurfaces( surfaceVals )
+
+def recombineLayers( layers ):
+
+    for layer in layers:
+
+        surfacesX = layer.surfacesz_xperp
+        surfacesY = layer.surfacesz_yperp
+        
+        recombineSurfaces( surfacesX )
+        recombineSurfaces( surfacesY )
+
+def recombine3DZone( zone ):
+
+    recombineZones( zone.zones2D )
+    recombineLayers( zone.layers )
+
+class Layer3D:
+
+    def __init__( self, zone1, zone2, Nx, Ny, transfinite = False ):
+
+        self.linesz = []
+        self.Nx = Nx
+        self.Ny = Ny
+        self.surfacesz_yperp = []
+        self.surfacesz_xperp = []
+        self.curveloopsz = []
+        self.surfaceloops = []
+        self.volumes = []
+
+        self.addLinesBetweenLayers( zone1, zone2 )
+        self.addSurfacesBetweenLayers( zone1, zone2 )
+        self.addVolumesBetweenLayers( zone1, zone2 )
+
+        if transfinite:
+            linesVec = [ self.linesz ]
+            setTransfiniteCurves( linesVec, 2 )
+            
+            setTransfiniteSurfaces( self.surfacesz_xperp )
+            setTransfiniteSurfaces( self.surfacesz_yperp )
+
+    def addLinesBetweenLayers( self, zone1, zone2 ):
+
+        for idy in range( self.Ny ):
+            for idx in range( self.Nx ):
+
+                indexval = idy * ( self.Nx ) + idx
+                self.linesz.append( gmsh.model.geo.addLine( zone1.pts[ indexval ], zone2.pts[ indexval ] ) )
+
+    def addSurfacesBetweenLayers( self, zone1, zone2 ):
+
+        for idy in range( self.Ny ):
+            for idx in range( self.Nx - 1 ):
+
+                indexval1 = idy * ( self.Nx - 1 ) + idx
+                indexval2 = idy * ( self.Nx ) + idx
+
+                downEdge = zone1.linesx[ indexval1 ]
+                upEdge = zone2.linesx[ indexval1 ]
+                zEdge1 = self.linesz[ indexval2 ]
+                zEdge2 = self.linesz[ indexval2 + 1 ]
+
+                linesVal = [ -downEdge, zEdge1, upEdge, -zEdge2 ]
+
+                cl = gmsh.model.geo.addCurveLoop( linesVal )
+                self.curveloopsz.append( cl )
+                
+                pl = gmsh.model.geo.addPlaneSurface( [ cl ])
+                self.surfacesz_yperp.append( pl )
+
+        for idy in range( self.Ny - 1 ):
+            for idx in range( self.Nx ):
+
+                indexval = idy * ( self.Nx ) + idx
+
+                downEdge = zone1.linesy[ indexval ]
+                upEdge = zone2.linesy[ indexval ]
+                zEdge1 = self.linesz[ indexval ]
+                zEdge2 = self.linesz[ indexval + self.Nx ]
+
+                linesVal = [ -downEdge, zEdge1, upEdge, -zEdge2 ]
+
+                print(linesVal)
+                cl = gmsh.model.geo.addCurveLoop( linesVal )
+                self.curveloopsz.append( cl )
+                
+                pl = gmsh.model.geo.addPlaneSurface( [ cl ])
+                self.surfacesz_xperp.append( pl )   
+
+    def addVolumesBetweenLayers( self, zone1, zone2 ):
+
+        Nx1 = self.Nx - 1
+        Nx2 = self.Nx
+
+        for idy in range( self.Ny - 1 ):
+            for idx in range( self.Nx - 1 ):
+
+                indexval1 = idy * Nx1 + idx
+                indexval2 = idy * Nx2 + idx
+
+                surface_zperp1 = zone1.surfaces[ indexval1 ]
+                surface_zperp2 = zone2.surfaces[ indexval1 ]
+                surface_yperp1 = self.surfacesz_yperp[ indexval1 ]
+                surface_yperp2 = self.surfacesz_yperp[ indexval1 + Nx1 ]
+                surface_xperp1 = self.surfacesz_xperp[ indexval2 ]
+                surface_xperp2 = self.surfacesz_xperp[ indexval2 + 1 ]
+
+                sl = gmsh.model.geo.addSurfaceLoop( [ surface_yperp1, surface_yperp2, surface_xperp1, surface_xperp2, \
+                                                     surface_zperp1, surface_zperp2 ] )
+                
+                vl = gmsh.model.geo.addVolume( [sl] )
+                self.volumes.append( vl )
+
+class Zone3D:
+
+    def __init__( self, zones, Nx, Ny, transfinite = False ):
+
+        nlayers = len( zones ) - 1
+
+        self.layers = []
+        self.zones2D = zones
+
+        for idx in range( nlayers ):
+            self.layers.append( Layer3D( zones[idx], zones[idx + 1], Nx, Ny, transfinite = transfinite ) )
+
+class Zone2D:
+
+    def __init__( self, pointSet, xoffset, yoffset, zoffset = 0, transfinite = False ):
 
         self.pts = []
         self.Nx = pointSet[0]
         self.Ny = pointSet[1]
         self.h = pointSet[2]
+
+        self.xoffset = xoffset
+        self.yoffset = yoffset
+        self.zoffset = zoffset
         
-        self.addPointSet( xoffset, yoffset )
+        self.addPointSet( xoffset, yoffset, zoffset )
 
         self.linesx = []
         self.linesy = []
-        self.addLinesInZone( pointSet )
+        self.addLinesInZone( )
 
         self.curveloops = []
         self.surfaces = []
-        self.addSurfaces( pointSet )
+        self.addSurfaces( )
 
-    def addPointSet( self, xoffset, yoffset ):
+        if transfinite:
+            linesVec = [ self.linesx, self.linesy ]
+            setTransfiniteCurves( linesVec, 2 )
+            setTransfiniteSurfaces( self.surfaces )
+
+    def addPointSet( self, xoffset, yoffset, zval = 0 ):
 
         yval = yoffset
         xval = xoffset
@@ -105,21 +244,21 @@ class Zone:
             for xidx in range( self.Nx ):
 
                 xval = xoffset + xidx * self.h
-                self.pts.append( gmsh.model.geo.addPoint( xval, yval, 0, self.h ) )
+                self.pts.append( gmsh.model.geo.addPoint( xval, yval, zval, self.h ) )
 
-    def addLinesInZone( self, pointSet ):
+    def addLinesInZone( self, startIdx = 0 ):
 
         for idy in range( self.Ny ):
             for idx in range( self.Nx - 1 ):
 
-                indexval = idy * ( self.Nx ) + idx
+                indexval = startIdx + idy * ( self.Nx ) + idx
 
                 self.linesx.append( gmsh.model.geo.addLine( self.pts[ indexval ], self.pts[ indexval + 1 ] ) )
 
         for idy in range( self.Ny - 1 ):
             for idx in range( self.Nx ):
 
-                indexval = idy * self.Nx + idx
+                indexval = startIdx + idy * self.Nx + idx
                 self.linesy.append( gmsh.model.geo.addLine( self.pts[ indexval ], self.pts[ indexval + self.Nx ] ) )
 
     def addSurfaces( self ):
@@ -173,3 +312,12 @@ class Zone:
             bdryPts = self.pts[ 0 : self.Nx ]   
 
         return bdryPts
+
+    def addLayer( self, xoffset, yoffset, zval ):
+
+        idxOffset = len( self.pts )
+
+        self.addPointSet( xoffset, yoffset, zval )
+        self.addLinesInZone( idxOffset )
+
+        return
--- src/regularMeshv2.py
+++ src/regularMeshv2.py
@@ -101,12 +101,26 @@ def doubleZoneMesh():
     gmshUtils.setTransfiniteSurfaces( zone2.surfaces )
 
     surfacePairs1 = [ ( 2, val ) for val in zone1.surfaces ]
-    ov2 = gmsh.model.geo.extrude( surfacePairs1, 0, 0, 1, numElements = [ Ny1 ], recombine = True )
+    ov1 = gmsh.model.geo.extrude( surfacePairs1, 0, 0, lc, numElements = [ 1 ], recombine = True )
 
     surfacePairs2 = [ ( 2, val ) for val in zone2.surfaces ]
-    ov2 = gmsh.model.geo.extrude( surfacePairs2, 0, 0, 1, numElements = [ Ny2 ], recombine = True )
+    ov2 = gmsh.model.geo.extrude( surfacePairs2, 0, 0, 2*lc, numElements = [ 1 ], recombine = True )
+
+    for entity in ov2:
+        if entity[0] != 2:
+            ov2.remove( entity )
+
+    norm = gmsh.model.getNormal( 1 )
+    print( norm )
+    # ov3 = gmsh.model.geo.extrude( ov2, 0, 0, 2*lc, numElements = [ 1 ], recombine = True )
 
     gmsh.model.geo.synchronize()
+
+    filename = "../build/ov2.txt"
+    with open( filename, "w" ) as filehandle:
+        vals = str(ov2)
+        print( vals, file = filehandle )
+
     gmshUtils.recombineSurfaces( zone1.surfaces )
     gmshUtils.recombineSurfaces( zone2.surfaces )
     gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 2)
@@ -125,6 +139,120 @@ def doubleZoneMesh():
 
     gmsh.finalize()
 
+def customExtrusionMeshRegular():
+
+    lvl = 0
+    Ndash = 4
+
+    lc = 1/( Ndash )
+    print(lc)
+
+    foldername = "/home/gaurav/gmshAutoScripts/build/"
+
+    N = int( 1/lc + 1 )
+
+    gmsh.initialize(sys.argv)
+    gmsh.model.add("t2")
+
+    pointSet = [N, N, lc]
+    xoffset = 0
+    yoffset = 0
+
+    zcoord = 0
+    zones = []
+    
+    for zidx in range(N):
+        
+        zcoord = zidx * lc
+        zone = gmshUtils.Zone2D( pointSet, xoffset, yoffset, zcoord, transfinite = True )
+        zones.append( zone )
+
+    zone3Dval = gmshUtils.Zone3D( zones, N, N, transfinite = True )
+
+    gmsh.model.geo.synchronize()
+
+    gmshUtils.recombine3DZone( zone3Dval )
+
+    gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 2)
+
+    gmsh.model.mesh.generate(2)
+
+    gmsh.option.setNumber("Mesh.MshFileVersion", 2)
+
+    regMeshFileName = foldername + "regularMesh_lvl" + str(lvl) + ".msh"
+    gmsh.write( regMeshFileName )
+
+    lvl = lvl + 1
+
+    if '-nopopup' not in sys.argv:
+        gmsh.fltk.run()
+
+    gmsh.finalize()
+
+
+def customExtrusionDoubleZoneMeshRegular():
+
+    lvl = 0
+
+    Nx1 = 5
+    Nx2 = 7
+    lc = 1/( Nx1 + 2*Nx2 - 1 )
+    Ny1 = int( 1/lc + 1 )
+    Ny2 = int( 1/2/lc + 1 )
+
+    foldername = "/home/gaurav/gmshAutoScripts/build/"
+
+    gmsh.initialize(sys.argv)
+    gmsh.model.add("t2")
+
+    pointSet1 = [Nx1, Ny1, lc]
+    xoffset1 = 0
+    yoffset1 = 0
+    zonesLeft = []
+
+    for zidx in range(3):
+        
+        zcoord = zidx * lc
+        zone = gmshUtils.Zone2D( pointSet1, xoffset1, yoffset1, zcoord, transfinite = True )
+        zonesLeft.append( zone )
+
+    pointSet2 = [Nx2, Ny2, 2*lc]
+    xoffset2 = (Nx1 + 1)*lc
+    yoffset2 = 0
+    zonesRight = []
+
+    for zidx in range(2):
+        
+        zcoord = zidx * 2 * lc
+        zone = gmshUtils.Zone2D( pointSet2, xoffset2, yoffset2, zcoord, transfinite = True )
+        zonesRight.append( zone )
+
+    zone3DLeft = gmshUtils.Zone3D( zonesLeft, Nx1, Ny1, transfinite = True )
+    zone3DRight = gmshUtils.Zone3D( zonesRight, Nx2, Ny2, transfinite = True )
+
+    gmsh.model.geo.synchronize()
+
+    gmshUtils.recombine3DZone( zone3DLeft )
+    gmshUtils.recombine3DZone( zone3DRight )
+
+    gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 2)
+
+    gmsh.model.mesh.generate(2)
+
+    gmsh.option.setNumber("Mesh.MshFileVersion", 2)
+
+    regMeshFileName = foldername + "regularMesh_lvl" + str(lvl) + ".msh"
+    gmsh.write( regMeshFileName )
+
+    lvl = lvl + 1
+
+    if '-nopopup' not in sys.argv:
+        gmsh.fltk.run()
+
+    gmsh.finalize()
+
 if __name__ == "__main__":
 
-    doubleZoneMesh()
+    # doubleZoneMesh()
+    customExtrusionDoubleZoneMeshRegular()
+    # customExtrusionMeshRegular()
